0e9d651ccb3d341dae0efef3d75a2a131d2274ec
Nicoly Avelino <65463907+NicolyAvelino@users.noreply.github.com>
2023-09-18 16:44:43 -0300

    atvd 06 concluida

--- atvd06/reduce.kt
+++ atvd06/reduce.kt
@@ -1,1 +1,17 @@
 // Faça um exemplo utilizando reduce
+data class Roupa(val modelo: String, val preco: Double)
+
+fun main() {
+    val roupas = listOf(
+        Roupa("Camiseta", 25.0),
+        Roupa("Calça Jeans", 85.0),
+        Roupa("Camisa Social", 60.0),
+        Roupa("Vestido", 90.0)
+    )
+
+    val somarPrecos = { total: Double, roupa: Roupa -> total + roupa.preco }
+
+    val precoTotal = roupas.map { it.preco }.reduce { total, preco -> total + preco }
+
+    println("O total é R$ $precoTotal")
+}
